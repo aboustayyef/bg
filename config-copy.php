9180eef14e39521f59d17e8ffe4e1b6bf2ff8eb5
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2013-09-03 11:53:04 +0300

    added config.php

---
+++ config-copy.php
@@ -0,0 +1,15 @@
+<?php
+// main constants configuration file
+ 
+// mains configuration
+error_reporting(0);
+define('DB_NAME', 'name of database');
+define('DB_USER', 'username');
+define('DB_PASS', 'password');
+define('DB_HOST', 'localhost');
+setlocale(LC_MONETARY, "en_US");
+ 
+// Base URL - does not contain a trailing slash
+define('BASE_URL', 'http://path/to/website');
+ 
+?>
