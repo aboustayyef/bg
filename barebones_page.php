01beadfdf9321b8850e7b5577d048c637fe29233
Mustapha <mustapha.hamoui@gmail.com>
2013-12-26 08:55:51 +0000

    removed legacy files

--- barebones_page.php
+++
@@ -1,8 +0,0 @@
-<?php session_start(); $thispage ="barebones" ; ?>
-
-<?php include("includes/site_header.php") ; ?> <!-- === Logo, contacts and searchbox === -->
-<?php include("includes/site_navigation.php");?> <!-- === the site's navigation === -->
-
-<div class="span-24" style ="background-color:blue ; height:200px"></div>
-
-<?php include("includes/site_footer.php"); ?>
