c539a7bec6947c5d0d275dcec0d182187447bc67
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-01-16 15:48:20 +0000

    small improvements

--- partials/slides.php
+++ partials/slides.php
@@ -1,8 +1,13 @@
 <div class="slideshow__wrapper">
 	<div class="slideshow__slides">
 	<div class="slideshow__info">
-			<h2 class="slideshow__header utlity__nomargins">Modern, Tasteful &amp;&nbsp;Durable</h2>
-			<p class="slideshow__text">Blue Gallery is Ghana's top destination for premium home and office furniture</p>		
+			<div class="layout__inner">
+				<h2 class="slideshow__header utlity__nomargins">Modern, Tasteful &amp;&nbsp;Durable</h2>
+				<p class="slideshow__text">Blue Gallery is Ghana's top destination for premium home and office furniture</p>					
+				<a href="#contact" class="slideshow__button">
+					Find Us
+				</a>
+			</div>
 	</div>
 	</div>
 </div>
